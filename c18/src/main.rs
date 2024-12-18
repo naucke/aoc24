ac3272684b45fb99534154dd8f6c97f73deff6f0
Jakob Naucke <jakob.naucke@posteo.de>
2024-12-18 23:51:06 +0100

    18b

--- c18/src/main.rs
+++ c18/src/main.rs
@@ -1,4 +1,5 @@
 use pathfinding::prelude::astar;
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::fs::File;
 use std::io::{self, BufRead};
@@ -6,7 +7,7 @@ use std::io::{self, BufRead};
 const DEPTH: usize = 1024;
 const DIM: i16 = 70;
 
-#[derive(Clone, Eq, Hash, PartialEq)]
+#[derive(Clone, Debug, Eq, Hash, PartialEq)]
 struct Pos(i16, i16);
 
 impl Pos {
@@ -17,22 +18,26 @@ impl Pos {
 
     fn successors(&self, blocked: &[Pos]) -> Vec<(Pos, i16)> {
         let &Pos(x, y) = self;
-        let succ: HashSet<_> = [
+        let surround = [
             (x > 0, Pos(x - 1, y)),
             (x < DIM, Pos(x + 1, y)),
             (y > 0, Pos(x, y - 1)),
             (y < DIM, Pos(x, y + 1)),
-        ]
-        .iter()
-        .filter(|(b, _)| *b)
-        .map(|(_, p)| p)
-        .cloned()
-        .collect();
-        return succ
-            .difference(&(blocked.into_iter().cloned().collect()))
-            .cloned()
-            .map(|x| (x, 1))
-            .collect();
+        ];
+        let succ = surround.iter().filter(|(b, _)| *b).map(|(_, p)| p);
+        let succ_set: HashSet<_> = succ.cloned().collect();
+        let binding = blocked.into_iter().cloned().collect();
+        let diff = succ_set.difference(&binding);
+        return diff.cloned().map(|x| (x, 1)).collect();
+    }
+
+    fn search(&self, blocked: &[Pos]) -> Option<(Vec<Pos>, i16)> {
+        return astar(
+            self,
+            |p| p.successors(blocked),
+            |p| p.distance(),
+            |p| *p == Pos(DIM, DIM),
+        );
     }
 }
 
@@ -47,23 +52,23 @@ fn collect_blocked(blocked: Vec<String>) -> Vec<Pos> {
 }
 
 fn task_a(blocked: &Vec<Pos>) -> i16 {
-    return astar(
-        &Pos(0, 0),
-        |p| p.successors(&blocked[..=DEPTH]),
-        |p| p.distance(),
-        |p| *p == Pos(DIM, DIM),
-    )
-    .unwrap()
-    .1;
+    return Pos(0, 0).search(&blocked[..DEPTH]).unwrap().1;
+}
+
+fn task_b(blocked: &Vec<Pos>) -> &Pos {
+    let idx = (0..blocked.len())
+        .collect::<Vec<_>>()
+        .binary_search_by(|i| match Pos(0, 0).search(&blocked[..*i]) {
+            Some(_) => Ordering::Less,
+            _ => Ordering::Greater,
+        })
+        .unwrap_err();
+    return &blocked[idx - 1];
 }
 
 fn main() {
-    let blocked = collect_blocked(
-        io::BufReader::new(File::open("input").unwrap())
-            .lines()
-            .into_iter()
-            .map(|s| s.unwrap())
-            .collect(),
-    );
+    let lines = io::BufReader::new(File::open("input").unwrap()).lines();
+    let blocked = collect_blocked(lines.into_iter().map(|s| s.unwrap()).collect());
     println!("{}", task_a(&blocked));
+    println!("{:?}", task_b(&blocked));
 }
