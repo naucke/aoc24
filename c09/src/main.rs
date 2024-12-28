b686e866b23b8ac5e03492945c2f0c59884f9414
Jakob Naucke <jakob.naucke@posteo.de>
2024-12-28 18:25:03 +0100

    refactor for 9a, 9b

--- c09/src/main.rs
+++ c09/src/main.rs
@@ -4,41 +4,42 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::iter;
 
+fn chk(fs: &Vec<i32>) -> usize {
+    let chk = fs.iter().enumerate().filter(|(_, &b)| b >= 0);
+    chk.map(|(i, &b)| i * (b as usize)).sum()
+}
+
 fn main() {
     let lines = io::BufReader::new(File::open("input").unwrap()).lines();
     let line_vec: Vec<_> = lines.collect::<Result<_, _>>().unwrap();
-    let input: Vec<_> = line_vec[0]
-        .chars()
-        .map(|c| c.to_digit(10).unwrap() as usize)
-        .collect();
+    let input_c = line_vec[0].chars();
+    let input: Vec<_> = input_c.map(|c| c.to_digit(10).unwrap() as usize).collect();
 
-    let mut fs = Vec::new();
-    let mut free_blk = VecDeque::new();
+    let mut fs_a = Vec::new();
+    let mut free_c = VecDeque::new();
     let mut id = 0;
     for (&taken, &free) in input.iter().tuples() {
-        fs.extend(iter::repeat(id).take(taken));
-        let len = fs.len();
-        free_blk.extend(len..len + free);
-        fs.extend(iter::repeat(-1).take(free));
+        fs_a.extend(iter::repeat(id).take(taken));
+        let len = fs_a.len();
+        free_c.extend(len..len + free);
+        fs_a.extend(iter::repeat(-1).take(free));
         id += 1;
     }
     if input.len() % 2 == 1 {
-        fs.extend(iter::repeat(id).take(input[input.len() - 1]));
+        fs_a.extend(iter::repeat(id).take(input[input.len() - 1]));
     }
 
-    let mut right = fs.len() - 1;
-    while let Some(free) = free_blk.pop_front() {
-        while fs[right] < 0 {
+    let mut right = fs_a.len() - 1;
+    while let Some(free) = free_c.pop_front() {
+        while fs_a[right] < 0 {
             right -= 1;
         }
         if free >= right {
             break;
         }
-        fs[free] = fs[right];
-        fs[right] = -1;
+        fs_a[free] = fs_a[right];
+        fs_a[right] = -1;
     }
 
-    let chk = fs.iter().take(right + 1).enumerate();
-    let a: usize = chk.map(|(i, &b)| i * (b as usize)).sum();
-    println!("{}", a);
+    println!("{}", chk(&fs_a));
 }
